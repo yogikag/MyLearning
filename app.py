5f12e7397b13d5543d2fcac1354c17a579e2eec9
Kag <Yogesh_Kag@Dellteam.com>
2021-04-06 09:29:49 +0530

    Flask Mongo Source Code

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/yogi"
+mongo = PyMongo(app)
